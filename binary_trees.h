7d7eb4f2a50ddbcdcaa6f93e87d5a4bb6c9af949
joshua onwuka <contactuche02@gmail.com>
2021-12-01 08:09:54 -0800

    header file

---
+++ binary_trees.h
@@ -0,0 +1,40 @@
+#ifndef _BINARY_TREES_
+#define _BINARY_TREES_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+/* Basic binary Tree */
+typedef struct binary_tree_s binary_tree_t;
+
+/* Binary search Tree */
+typedef struct binary_tree_s bst_t;
+
+/* AVL Tree */
+typedef struct binary_tree_s avl_t;
+
+/* MAx Binary heap */
+typedef struct binary_tree_s heap_t;
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
+static size_t _height(const binary_tree_t *tree);
+
+#endif
