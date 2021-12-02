e0c8e5c6b088351d1b2254867c5d619b06080f09
joshua onwuka <contactuche02@gmail.com>
2021-12-02 00:59:43 -0800

    update header file

--- binary_trees.h
+++ binary_trees.h
@@ -36,5 +36,5 @@ typedef struct binary_tree_s heap_t;
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
 static size_t _height(const binary_tree_t *tree);
-
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 #endif
